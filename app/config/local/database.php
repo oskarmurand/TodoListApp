2bbeee65efd9ab53982e72e25817b8b13104c2b8
Allan Tatter <allan.tatter@gmail.com>
2015-03-23 10:34:43 +0200

    Add local environment

---
+++ app/config/local/database.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'connections' => array(
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'oskar_todo',
+			'username'  => 'root',
+			'password'  => 'root',
+			'port'			=> '3306',
+		),
+    ),
+];
